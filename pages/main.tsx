d6f10d2db68ca5007759580e6b90f72bcd6a4b96
Sean Boateng <seanboateng2@gmail.com>
2023-12-20 22:35:59 -0500

    Testing

---
+++ pages/main.tsx
@@ -0,0 +1,13 @@
+import Accordion from "./components/accordion";
+
+export default function Main() {
+    return (
+      <main className="artboard">
+        <Accordion title="Hello" content="Yurrrr yurrrrrrrrrrr yurrrrrrrrr urrrrr"/>
+        <Accordion title="Hello" content="Yurrrr yurrrrrrrrrrr yurrrrrrr"/>
+        <Accordion title="Hello" content="Yurrrr yurrrrrrrrrrr yurrrrrrrrr urrrrr"/>
+        
+      </main>
+    )
+  }
+  
