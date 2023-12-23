886b075442aa964bca5c87f642ff0121e7dc5123
Sean Boateng <seanboateng2@gmail.com>
2023-12-22 19:59:24 -0500

    Removed comments

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,7 @@
 export default function Home() {
   return (
     <main className="backround">
-<style>
-          {`
-            @import url('https://fonts.googleapis.com/css2?family=Poppins&family=Work+Sans:wght@700&display=swap');
-            body {
-              font-family: 'Poppins', sans-serif;
-            }
-          `}
-        </style>
+
       
     </main>
   )
